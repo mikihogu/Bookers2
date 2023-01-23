ac6b9e5f5acf561781701c62523b6a5049bd807c
EC2 Default User <ec2-user@ip-172-31-14-145.ap-northeast-1.compute.internal>
2023-01-23 09:44:42 +0000

    [Update] migrationファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_101057) do
+ActiveRecord::Schema.define(version: 2023_01_17_070754) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2023_01_18_101057) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.text "introduction"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -57,7 +58,6 @@ ActiveRecord::Schema.define(version: 2023_01_18_101057) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
